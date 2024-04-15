8b299aa8605faf7509716d4274698028e4791d9a
Aaron <104621430+X-aaron-X@users.noreply.github.com>
2024-04-15 23:48:58 +0200

    Optimatizado consulta db

--- ConsultadorHacendado/src/consultadorhacendado/ConsultadorHacendado.java
+++ ConsultadorHacendado/src/consultadorhacendado/ConsultadorHacendado.java
@@ -3,131 +3,100 @@ package consultadorhacendado;
 import java.sql.*;
 import java.util.Scanner;
 
-/**
- *
- * @author Aaron
- */
 public class ConsultadorHacendado {
-    static Connection conn = null;
-    static Statement stmt;
-    static ResultSet rs;
+    static Connection conexion = null;
     static String url = "jdbc:mysql://localhost:3307/ciclistas";
     static String user = "root";
-    static String password = "";
-    static String tabla = "";
-    static String pk = "";
+    static String pass = "";
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, user, pass);
+        }
+        catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error de la conexion de la base de datos");
+        }
 
-        System.out.println("MIntroduce dato a consultar: ");
+        Scanner escaner = new Scanner(System.in);
+
+        System.out.println("Introduce dato a consultar: ");
         System.out.println("1. Ciclista");
         System.out.println("2. Equipo");
         System.out.println("3. Etapa");
-        
-        int opciones = scanner.nextInt();
-        
-        switch (opciones) { 
+
+        int opciones = escaner.nextInt();
+        escaner.nextLine();
+
+        switch (opciones) {
             case 1 -> {
                 System.out.print("Introduce el dorsal: ");
-                int dorsal = scanner.nextInt();
-                tabla = "ciclista";
-                pk = "dorsal";
-                
-                consulta(dorsal);
-            }
+                int dorsal = escaner.nextInt();
+
+                consulta("ciclista", "dorsal", String.valueOf(dorsal));
+            }   
             case 2 -> {
                 System.out.print("Introduce el nombre del equipo: ");
-                String nombreEquipo = scanner.nextLine();
-                
-                tabla = "equipo";
-                pk = "nomequipo";
-                
-                consultaString(nombreEquipo);
+                String nombreEquipo = escaner.nextLine();
+
+                consulta("equipo", "nomequipo", nombreEquipo);
             }
             case 3 -> {
                 System.out.print("Introduce la etapa: ");
-                int etapa = scanner.nextInt();
-                
-                tabla = "etapa";
-                pk = "numetapa";
-                
-                consulta(etapa);
+                int etapa = escaner.nextInt();
+
+                consulta("etapa", "numetapa", String.valueOf(etapa));
             }
             default -> System.out.println("La opcion escogida es incorrecta");
         }
     }
     
-    public static void consulta(int id) {
-        // Importar driver
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e) {
-            System.out.println("Fallo al importal el driver");
-        }
+    /**
+     * Realiza una consulta a la base de datos. El PK puede ser INT o VARCHAR.
+     * @param tabla Nombre de la tabla.
+     * @param pk Primary key de la tabla.
+     * @param id Identificador para la consulta.
+    */
+    public static void consulta(String tabla, String pk, String id) {
+        String consulta = "SELECT * FROM " + tabla + " WHERE " + pk + " = ?";
         
-        // Trabajo con base de datos
-        try {
-            conn = DriverManager.getConnection(url, user, password);
-            stmt = conn.createStatement();
+        try (PreparedStatement comprobar = conexion.prepareStatement(consulta)) {
+            // Comprobamos si "id" solo tiene digitos
+            if (id.matches("\\d+")) {
+                comprobar.setInt(1, Integer.parseInt(id));
+            }
+            else {
+                comprobar.setString(1, id);
+            }
             
-            //stmt.execute("CREATE DATABASE PROBANDO");
-            rs = stmt.executeQuery("SELECT * FROM " + tabla + " WHERE " + pk + "=" + id);
-                while (rs.next()) {
-                    
-                    ResultSetMetaData metaData = rs.getMetaData();
-                    int columnCount = metaData.getColumnCount();
-
-                    for (int i = 1; i <= columnCount; i++) {
-                        Object value = rs.getObject(i);
-                        String columnName = metaData.getColumnName(i);
-                        System.out.println(columnName + ": " + value);
-                    }
-                    
-                    System.out.println();
-                }
-            rs.close();
-            stmt.close();
+            // Ejecutamos la consulta y lo guardamos en resultados
+            try (ResultSet resultados = comprobar.executeQuery()) {
+                imprimirResultados(resultados);
+            }
         }
         catch (SQLException e) {
-            System.out.println("Fallo al hacer las consultas");
+            System.out.println("Error al ejecutar la consulta");
         }
     }
     
-    public static void consultaString(String id) {
-       // Importar driver
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e) {
-            System.out.println("Fallo al importal el driver");
-        }
-        
-        // Trabajo con base de datos
-        try {
-            conn = DriverManager.getConnection(url, user, password);
-            stmt = conn.createStatement();
-            
-            rs = stmt.executeQuery("SELECT * FROM " + tabla + " WHERE " + pk + "=" + id + "");
-                while (rs.next()) {
-                    
-                    ResultSetMetaData metaData = rs.getMetaData();
-                    int columnCount = metaData.getColumnCount();
+    /**
+     * Imprime los resultados de una consulta a la base de datos. Mostrando todas las columas de la tabla.
+     * @param resultados Los resultados de la consulta a imprimir.
+     * @throws SQLException Si ocurre un error al acceder a los resultados.
+    */
+    public static void imprimirResultados(ResultSet resultados) throws SQLException {
+        ResultSetMetaData datos = resultados.getMetaData();
+        int numColumnas = datos.getColumnCount();
 
-                    for (int i = 1; i <= columnCount; i++) {
-                        Object value = rs.getObject(i);
-                        String columnName = metaData.getColumnName(i);
-                        System.out.println(columnName + ": " + value);
-                    }
-                    
-                    System.out.println();
-                }
-            rs.close();
-            stmt.close();
-        }
-        catch (SQLException e) {
-            System.out.println("Fallo al hacer las consultas");
+        while (resultados.next()) {
+            for (int i = 1; i <= numColumnas; i++) {
+                Object valor = resultados.getObject(i);
+                String nombreColumna = datos.getColumnName(i);
+                
+                System.out.println(nombreColumna + ": " + valor);
+            }
+            
+            System.out.println();
         }
     }
 }
