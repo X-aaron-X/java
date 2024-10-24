644bdbc546c258b4d77feddc350edffbc58ec7ef
Aaron <104621430+X-aaron-X@users.noreply.github.com>
2024-10-24 20:07:14 +0200

    Ejercicios basicos java

--- estructurada2023/MiPrimirProyecto/src/miprimirproyecto/MiPrimirProyecto.java
+++ estructurada2023/MiPrimirProyecto/src/miprimirproyecto/MiPrimirProyecto.java
@@ -1,29 +1,18 @@
 package miprimirproyecto;
 
 import static java.io.IO.println;
-import java.util.Random;
 import javax.swing.JOptionPane;
 
-
 public class MiPrimirProyecto {
-    static void main() {
-        Random random = new Random();
-
-        int numeroAleatorio = random.nextInt(100);
-        println(numeroAleatorio);
+    public static void main() {
+        parOIimpar();
+    }
+    
+    public static void parOIimpar() {
+        int num;
         
-        try {
-            int edad = Integer.parseInt(JOptionPane.showInputDialog("Introduce tu edad:"));
-            
-            switch (edad) {
-                case 0, 1, 2 -> println("Eres un bebe");
-                case 3, 4, 5 -> println("Eres un niño pequeño");
-                default -> println("Edad no valida");
-            }
-        }
-        catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Por favor, introduce un número válido.");
-            return;
-        }
+        num = Integer.parseInt(JOptionPane.showInputDialog("Introduce número para saber si es par o impar"));
+        
+        println((num % 2 == 0) ? "El número " + num + " es par" : "El número " + num + " es impar");
     }
 }
